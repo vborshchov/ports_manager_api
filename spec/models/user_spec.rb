c2d23425bc258c33bb3d29ed36e34f9b99b34b92
Victor Borshchov <victor.borshchov@gmail.com>
2015-11-09 10:54:56 +0200

    Use shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
-
-
 describe User do
   before { @user = FactoryGirl.build(:user) }
 
@@ -15,4 +10,9 @@ describe User do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
 end
