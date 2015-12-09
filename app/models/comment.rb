91cb9ade35c853810967a699604680fbc5e8da75
Victor Borshchov <victor.borshchov@gmail.com>
2015-12-09 09:03:24 +0200

    Adds validation to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :port
+
+  validates_presence_of :body
 end
--- app/models/port.rb
+++ app/models/port.rb
@@ -1,4 +1,5 @@
 class Port < ActiveRecord::Base
   belongs_to :node
   belongs_to :customer
+  has_many :comments
 end
