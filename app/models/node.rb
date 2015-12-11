6a620680da5d6768b3fe0bfa07598b2d9cef3666
Victor Borshchov <victor.borshchov@gmail.com>
2015-12-11 09:22:13 +0200

    Refactors 'get_ports' method

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,4 +1,5 @@
 class Node < ActiveRecord::Base
+  require 'snmp'
   # include SNMP
 
   belongs_to :location
@@ -10,4 +11,37 @@ class Node < ActiveRecord::Base
   scope :ciscos, -> { where(type: 'Cisco') }
   scope :dlinks, -> { where(type: 'Dlink') }
   scope :ztes, -> { where(type: 'Zte') }
+
+  def get_ports
+    @ports_arr = []
+    host = self.ip
+    host_community = (self.class.name == "Cisco") ? "sw3400" : "public"
+    ifTable_columns = ["ifDescr", "ifAdminStatus", "ifOperStatus", "ifAlias"]
+    begin
+      SNMP::Manager.open(:Host => host, :Community => host_community) do |manager|
+        response = manager.get(["sysName.0", "sysUpTime.0"])
+        response.each_varbind do |vb|
+            puts "#{vb.name.to_s[/(?<=\:{2}).*(?=\.)/].split(/(?=[A-Z])/).join(" ")}  #{vb.value.to_s}"
+        end
+        manager.walk(ifTable_columns) do |row|
+          port_attributes = {}
+          state_index = -2 # this id for index in array of states
+          row.each do |vb|
+            case vb.name.to_s
+              when /Desc/
+                port_attributes[:name] = vb.value.to_s
+              when /AdminStatus|OperStatus/
+                state_index += vb.value.to_s.to_i
+              when /Alias/
+                port_attributes[:description] = vb.value unless vb.value == ''
+            end
+            port_attributes[:state] = Port::STATES[state_index]
+          end
+          @ports_arr << port_attributes
+        end
+      end
+    rescue
+    end
+    puts @ports_arr
+  end
 end
