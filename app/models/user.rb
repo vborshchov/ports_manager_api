bc3c132be1977b5af313e2f5cd7ab8001de16ac9
Victor Borshchov <victor.borshchov@gmail.com>
2015-12-30 16:46:43 +0200

    Makes default locale to be ukrainian

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   before_create :generate_authentication_token!
   validates :auth_token, uniqueness: true
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -19,6 +19,7 @@ RailsAdmin.config do |config|
   ## == PaperTrail ==
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
+  config.excluded_models << "User"
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
   config.actions do
