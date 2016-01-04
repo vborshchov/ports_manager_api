5de7101e494634cf45647d8e622cea634959a8f9
Victor Borshchov <victor.borshchov@gmail.com>
2016-01-04 14:47:12 +0200

    Fixes problem with Kaminari: https://github.com/sferik/rails_admin/issues/2503
    Going to the history page (using PaperTrail for the auditing) and showing all history throws an error:
    
    undefined method `total_count' for #PaperTrail::Version::ActiveRecord_Relation
    (example url: http://localhost:3000/admin/port/history?all=true)

---
+++ config/initializers/kaminari_config.rb
@@ -0,0 +1,16 @@
+Kaminari.configure do |config|
+  # config.default_per_page = 25
+  # config.max_per_page = nil
+  # config.window = 4
+  # config.outer_window = 0
+  # config.left = 0
+  # config.right = 0
+  # config.page_method_name = :page
+  # config.param_name = :page
+end
+
+module ActiveRecord
+  class Relation
+    alias_method :total_count, :count
+  end
+end
