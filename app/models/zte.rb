3173470899075167ad53807875aaaa48eb7f54fd
Victor Borshchov <victor.borshchov@gmail.com>
2016-01-13 15:56:17 +0200

    Uses variable to get telnet output

--- app/models/zte.rb
+++ app/models/zte.rb
@@ -29,44 +29,49 @@ class Zte < Node
           "Host" => host,
           "Timeout" => 1,
           "Waittime" => 0.5,
-          "Prompt" => /[#>:]/n,
-          "Output_log" => "output_log.txt"
+          "Prompt" => /[#>:]/n#,
+          # "Output_log" => "output_log.txt"
         )
         tn.waitfor(/:/) do |banner|
           zte_model_regex = Regexp.new("#{zte_model}|ZTE")
           if banner.match(zte_model_regex)
+            response = []
             tn.cmd("#{ENV["ZTE_USERNAME"]}\n#{ENV["ZTE_PASSWORD"]}") #{ |c| print c }
             port_number.to_i.times do |i|
               port = zte_model =~ /5250/ ? "1/#{i+1}" : i+1
-              tn.cmd("show port #{port}")
-              tn.cmd(" ")
+              response << tn.cmd("show port #{port}")
+              response << tn.cmd(" ")
             end
-            tn.cmd("n")
-            tn.cmd("n")
-            tn.cmd("exit")
+            response << tn.cmd("n")
+            response << tn.cmd("n")
+            response << tn.cmd("exit")
           end
         end
         tn.close
 
-        @ports_info_arr = parse_log("output_log.txt")
+        # @ports_info_arr = parse_log("output_log.txt")
 
       rescue Exception => e  
         puts e.message  
         puts e.backtrace.inspect
       ensure
         # Remove unnecessary log file
-        system('rm output_log.txt')
+        # system('rm output_log.txt')
       end
       @ports_info_arr
     end
   end
 
   # Parsing log file
-  def parse_log(file)
+  def parse_log(response)
+  # def parse_log(file)
     lines = []
-    if File.exist?(file)
-      File.open(file, "r") do |f|
-        f.each do |line|
+    # if File.exist?(file)
+    #   File.open(file, "r") do |f|
+    response
+        .split("\n")
+        .reject(&:empty?)
+        .each do |line|
           case line
             when /PortId|Description|PortName|PortEnable/
               lines << line.match(/([A-Z]\w+(?= *:)) *: (\S+)\s/)[2]
@@ -74,8 +79,8 @@ class Zte < Node
               lines << line.match(/(?<=Link           : )(\w+)/)[1]
           end
         end
-      end
-    end
+    #   end
+    # end
     lines
         .slice_before(/\A1*\/*\d{1,2}\z/)
         .map do |id, description = "", port_enable, link|
