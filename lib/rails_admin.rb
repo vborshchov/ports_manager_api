f127076136bfb1bf7c0c37a08d485ccdcdcd7277
Victor Borshchov <victor.borshchov@gmail.com>
2016-05-27 12:28:51 +0300

    Adds pie and column charts

--- lib/rails_admin.rb
+++ lib/rails_admin.rb
@@ -17,6 +17,23 @@ module RailsAdmin
             #You can specify instance variables
             @data = Port.updated_per_day.drop(7)
 
+            @nodes = Node.group(:type).count.values
+
+            admin_down = Node.joins(:ports).where("ports.state = ?", "admin down").group(:type).count
+            down = Node.joins(:ports).where("ports.state = ?", "down").group(:type).count
+            up = Node.joins(:ports).where("ports.state = ?", "up").group(:type).count
+
+            @admin_down=[]
+            @down=[]
+            @up=[]
+            ['Cisco', 'Dlink', 'Huawei', 'Iskratel', 'Zte'].each do |node|
+              @admin_down << admin_down[node].to_i
+              @down << down[node].to_i
+              @up << up[node].to_i
+            end
+
+
+
             #You can access submitted params (just submit your form to the dashboard).
             if params[:xyz]
               @do_something = "here"
